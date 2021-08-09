cc2b111d9cc293e19a2597e7b989d6785bd557d1
Finomnis <finomnis@gmail.com>
2021-08-09 21:45:50 +0200

    Fix authenticationSaga formatting & leftover comments

--- src/features/pianod2/controller/authenticationSaga.ts
+++ src/features/pianod2/controller/authenticationSaga.ts
@@ -1,17 +1,16 @@
-// TODO
-// - Wait for authentication changed / socket initialized
-// - send authentication request
-// - repeat
-
 import { call, race, select, take } from "@redux-saga/core/effects";
 import pianod2_client from "../connection/pianod2_client";
 import { getCredentials } from "../store/selectors";
 import { credentialsChanged } from "../store/slices/credentials";
 import { connectionEstablished } from "../store/slices/websocket";
 
-function* authenticate(credentials: { username: string, password: string }) {
+function* authenticate(credentials: { username: string; password: string }) {
     try {
-        yield call(pianod2_client.execute_command.bind(pianod2_client), "authenticate", credentials);
+        yield call(
+            pianod2_client.execute_command.bind(pianod2_client),
+            "authenticate",
+            credentials
+        );
     } catch (e) {
         console.warn("Unable to authenticate:", e);
     }
@@ -20,8 +19,12 @@ function* authenticate(credentials: { username: string, password: string }) {
 export function* authenticationSaga() {
     while (true) {
         type RaceResult = {
-            connection_established: (ReturnType<typeof connectionEstablished>) | undefined,
-            credentials_changed: (ReturnType<typeof credentialsChanged>) | undefined,
+            connection_established:
+                | ReturnType<typeof connectionEstablished>
+                | undefined;
+            credentials_changed:
+                | ReturnType<typeof credentialsChanged>
+                | undefined;
         };
 
         const result: RaceResult = yield race({
@@ -33,7 +36,8 @@ export function* authenticationSaga() {
         const credentials_changed = result.credentials_changed;
 
         if (connection_established) {
-            const credentials: { username: string, password: string } = yield select(getCredentials);
+            const credentials: { username: string; password: string } =
+                yield select(getCredentials);
             yield call(authenticate, credentials);
         }
 
@@ -42,4 +46,4 @@ export function* authenticationSaga() {
             yield call(authenticate, credentials);
         }
     }
-};
+}
